654a1ffcb050b41a65ffa14018b6cbcab93b11ee
Nancy Singleton <Nancy.Singleton@softwire.com>
2024-01-02 14:50:20 +0000

    Display saved items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config
 
@@ -9,4 +10,5 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    items = get_items()
+    return render_template('index.html', items = items)
