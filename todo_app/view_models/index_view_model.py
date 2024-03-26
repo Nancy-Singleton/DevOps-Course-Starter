74cee7c9d7f0c86ba2e9a0f372046332850f2c7d
Nancy Singleton <nancy.singleton@softwire.com>
2024-03-26 17:48:06 +0000

    EX3: Add new placeholder properties

--- todo_app/view_models/index_view_model.py
+++ todo_app/view_models/index_view_model.py
@@ -9,3 +9,11 @@ class IndexViewModel:
     @property
     def done_items(self):
         return [x for x in self._items if x.is_done()]
+    
+    @property
+    def doing_items(self):
+        return []
+    
+    @property
+    def to_do_items(self):
+        return []
