7d7d430f7b70667e6f420bd6274ef15164809b66
Nancy Singleton <nancy.singleton@softwire.com>
2024-03-26 13:45:04 +0000

    EX2: Allow marking an item as completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, request
-from todo_app.data.trello_items import get_trello_items, add_trello_item
+from todo_app.data.trello_items import get_trello_items, add_trello_item, complete_trello_item
 
 from todo_app.flask_config import Config
 
@@ -16,4 +16,9 @@ def index():
 def add_item():
     title = request.form.get('title')
     add_trello_item(title)
+    return redirect('/')
+
+@app.route('/complete_item/<item_id>', methods=['POST'])
+def complete_item(item_id):
+    complete_trello_item(item_id)
     return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -38,3 +38,19 @@ def add_trello_item(title):
     body = {'name': title}
 
     requests.post(url, body)
+
+def complete_trello_item(item_id):
+    """
+    Moves the specified item to the "Done" list on the Trello board.
+
+    Args:
+        item_id: The id of the item.
+    """
+    done_list_id = os.getenv('TRELLO_DONE_LIST_ID')
+    key = os.getenv('TRELLO_API_KEY')
+    token = os.getenv('TRELLO_API_TOKEN')
+
+    url=f'https://api.trello.com/1/cards/{item_id}?key={key}&token={token}'
+    body = {'idList': done_list_id}
+
+    requests.put(url, body)
