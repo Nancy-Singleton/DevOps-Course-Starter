d5762f99436747ffe6f9ff3ce7a2c2a7693f3f46
Nancy Singleton <nancy.singleton@softwire.com>
2024-04-02 11:15:28 +0100

    EX3: Move routes into create_app

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,23 +7,24 @@ from todo_app.view_models.index_view_model import IndexViewModel
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+
+    @app.route('/')
+    def index():
+        items = get_trello_items()
+        item_view_model = IndexViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
+
+    @app.route('/item', methods=['POST'])
+    def add_item():
+        title = request.form.get('title')
+        add_trello_item(title)
+        return redirect('/')
+
+    @app.route('/complete_item/<item_id>', methods=['POST'])
+    def complete_item(item_id):
+        complete_trello_item(item_id)
+        return redirect('/')
+
     return app
 
 app = create_app()
-
-@app.route('/')
-def index():
-    items = get_trello_items()
-    item_view_model = IndexViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
-
-@app.route('/item', methods=['POST'])
-def add_item():
-    title = request.form.get('title')
-    add_trello_item(title)
-    return redirect('/')
-
-@app.route('/complete_item/<item_id>', methods=['POST'])
-def complete_item(item_id):
-    complete_trello_item(item_id)
-    return redirect('/')
