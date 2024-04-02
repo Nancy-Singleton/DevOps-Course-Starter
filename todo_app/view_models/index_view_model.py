493ffe3f37312e92cbfde8f87aad0b67e4a8ce2c
Nancy Singleton <nancy.singleton@softwire.com>
2024-04-02 09:41:02 +0100

    EX3: Reorder vm properties

--- todo_app/view_models/index_view_model.py
+++ todo_app/view_models/index_view_model.py
@@ -5,15 +5,15 @@ class IndexViewModel:
     @property
     def items(self):
         return self._items
-    
-    @property
-    def done_items(self):
-        return [x for x in self._items if x.is_done()]
-    
-    @property
-    def doing_items(self):
-        return []
-    
+
     @property
     def to_do_items(self):
         return []
+
+    @property
+    def doing_items(self):
+        return []
+
+    @property
+    def done_items(self):
+        return [x for x in self._items if x.is_done()]
