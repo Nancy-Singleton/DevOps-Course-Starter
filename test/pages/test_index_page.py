0e9581bbf968c07fef83120920eb55b978dd760a
Nancy Singleton <nancy.singleton@softwire.com>
2024-04-02 13:38:18 +0100

    EX3: Add new test

---
+++ test/pages/test_index_page.py
@@ -0,0 +1,18 @@
+import pytest
+from dotenv import load_dotenv, find_dotenv
+from todo_app import app
+
+@pytest.fixture
+def client():
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    # Create the new app.
+    test_app = app.create_app()
+
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
+
+def test_index_page(client):
+    response = client.get('/')
