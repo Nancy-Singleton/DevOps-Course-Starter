6df2a336c9a97abf136ad817d62bdce848279ef1
Nancy Singleton <nancy.singleton@softwire.com>
2024-04-02 09:32:26 +0100

    EX3: Add failing tests

--- test/view_models/test_index_view_model.py
+++ test/view_models/test_index_view_model.py
@@ -3,17 +3,36 @@ from todo_app.view_models.index_view_model import IndexViewModel
 
 def test_done_items_property_returns_done_items():
     # arrange
-    item_1 = Item(1, 'Completed item 1', 'Done')
-    item_2 = Item(2, 'Completed item 2', 'Done')
-    item_3 = Item(3, 'To do item 1', 'To Do')
-    item_3 = Item(4, 'To do item 2', 'To Do')
-    item_4 = Item(5, 'To do item 3', 'To Do')
-    item_4 = Item(6, 'To do item 3', 'To Do')
-
-    view_model = IndexViewModel([item_1, item_2, item_3, item_4])
+    items = create_items_with_statuses(['Done', 'Done', 'To Do', 'To Do', 'To Do', 'Doing'])
+    view_model = IndexViewModel(items)
 
     # act
     result = view_model.done_items
 
     # assert
     assert len(result) == 2
+
+def test_doing_items_property_returns_doing_items():
+    # arrange
+    items = create_items_with_statuses(['Done', 'Done', 'To Do', 'To Do', 'To Do', 'Doing'])
+    view_model = IndexViewModel(items)
+
+    # act
+    result = view_model.done_items
+
+    # assert
+    assert len(result) == 1
+
+def test_to_do_items_property_returns_to_do_items():
+    # arrange
+    items = create_items_with_statuses(['Done', 'Done', 'To Do', 'To Do', 'To Do', 'Doing'])
+    view_model = IndexViewModel(items)
+
+    # act
+    result = view_model.done_items
+
+    # assert
+    assert len(result) == 3    
+
+def create_items_with_statuses(statuses):
+    return [Item(idx + 1, 'An item description', status) for idx, status in enumerate(statuses)]
