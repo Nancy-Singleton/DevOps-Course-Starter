742d4e12aa1410d5d39143c3213cfae92fe483e2
Nancy Singleton <nancy.singleton@softwire.com>
2024-04-02 09:36:44 +0100

    EX3: Reorder tests

--- test/view_models/test_index_view_model.py
+++ test/view_models/test_index_view_model.py
@@ -1,7 +1,7 @@
 from todo_app.models.item import Item
 from todo_app.view_models.index_view_model import IndexViewModel
 
-def test_done_items_property_returns_done_items():
+def test_to_do_items_property_returns_to_do_items():
     # arrange
     items = create_items_with_statuses(['Done', 'Done', 'To Do', 'To Do', 'To Do', 'Doing'])
     view_model = IndexViewModel(items)
@@ -10,7 +10,7 @@ def test_done_items_property_returns_done_items():
     result = view_model.done_items
 
     # assert
-    assert len(result) == 2
+    assert len(result) == 3    
 
 def test_doing_items_property_returns_doing_items():
     # arrange
@@ -23,7 +23,7 @@ def test_doing_items_property_returns_doing_items():
     # assert
     assert len(result) == 1
 
-def test_to_do_items_property_returns_to_do_items():
+def test_done_items_property_returns_done_items():
     # arrange
     items = create_items_with_statuses(['Done', 'Done', 'To Do', 'To Do', 'To Do', 'Doing'])
     view_model = IndexViewModel(items)
@@ -32,7 +32,7 @@ def test_to_do_items_property_returns_to_do_items():
     result = view_model.done_items
 
     # assert
-    assert len(result) == 3    
+    assert len(result) == 2
 
 def create_items_with_statuses(statuses):
     return [Item(idx + 1, 'An item description', status) for idx, status in enumerate(statuses)]
