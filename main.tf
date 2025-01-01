303e48bf934e71535f12e8cf227f19d1fdf8552a
Nancy Singleton <nancysingleton_@hotmail.co.uk>
2025-01-01 18:38:14 +0000

    EX12: Use azure backend for tf state

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">= 3.8"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "cohort32-33_NanSin_ProjectExercise"
+    storage_account_name = "nansinstorageaccount01"
+    container_name       = "nansin-container-01"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
