e4139341c74e391052e5ed4c8a403d1da0941a6e
Nancy Singleton <nancy.singleton@softwire.com>
2025-01-02 17:20:38 +0000

    EX13: Add logging

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,22 +7,29 @@ from todo_app.view_models.index_view_model import IndexViewModel
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    app.logger.setLevel(app.config['LOG_LEVEL'])
 
     @app.route('/')
     def index():
         items = get_items()
         item_view_model = IndexViewModel(items)
+
+        app.logger.info("Homepage loaded")
         return render_template('index.html', view_model=item_view_model)
 
     @app.route('/item', methods=['POST'])
     def add_item():
         title = request.form.get('title')
         save_item(title)
+
+        app.logger.info("Item added")
         return redirect('/')
 
     @app.route('/complete_item/<item_id>', methods=['POST'])
     def complete_item(item_id):
         mark_item_done(item_id)
+
+        app.logger.info("Item completed")
         return redirect('/')
 
     return app
