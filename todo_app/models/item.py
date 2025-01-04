0632960d548b3b64379965ab7e31e8e963f25787
Nancy Singleton <nancy.singleton@softwire.com>
2025-01-04 21:01:59 +0000

    Add table styling

--- todo_app/models/item.py
+++ todo_app/models/item.py
@@ -3,12 +3,3 @@ class Item:
         self.id = id
         self.name = name
         self.status = status
-    
-    def is_to_do(self):
-        return self.status == 'To Do'
-              
-    def is_doing(self):
-        return self.status == 'Doing'
-  
-    def is_done(self):
-        return self.status == 'Done'
--- todo_app/view_models/index_view_model.py
+++ todo_app/view_models/index_view_model.py
@@ -5,15 +5,3 @@ class IndexViewModel:
     @property
     def items(self):
         return self._items
-
-    @property
-    def to_do_items(self):
-        return [x for x in self._items if x.is_to_do()]
-
-    @property
-    def doing_items(self):
-        return [x for x in self._items if x.is_doing()]
-
-    @property
-    def done_items(self):
-        return [x for x in self._items if x.is_done()]
