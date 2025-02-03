a9d72df8bce57eceaab988fb3ea7af31e96a18f4
Nancy-Singleton <104018299+Nancy-Singleton@users.noreply.github.com>
2025-02-03 11:16:41 +0000

    Exercise 13 (#13)
    * EX13: Change default local database name
    
    * EX13: Add logging
    
    * EX13: Send logs to Loggly

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, redirect, request
+from loggly.handlers import HTTPSHandler
+from logging import Formatter
 from todo_app.data.items_repository import get_items, save_item, mark_item_done
 
 from todo_app.flask_config import Config
@@ -7,22 +9,36 @@ from todo_app.view_models.index_view_model import IndexViewModel
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    app.logger.setLevel(app.config['LOG_LEVEL'])
+
+    if app.config['LOGGLY_TOKEN'] is not None:
+        handler = HTTPSHandler(f'https://logs-01.loggly.com/inputs/{app.config["LOGGLY_TOKEN"]}/tag/todo-app')
+        handler.setFormatter(
+            Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
+        )
+        app.logger.addHandler(handler)
 
     @app.route('/')
     def index():
         items = get_items()
         item_view_model = IndexViewModel(items)
+
+        app.logger.info("Homepage loaded")
         return render_template('index.html', view_model=item_view_model)
 
     @app.route('/item', methods=['POST'])
     def add_item():
         title = request.form.get('title')
         save_item(title)
+
+        app.logger.info("Item added")
         return redirect('/')
 
     @app.route('/complete_item/<item_id>', methods=['POST'])
     def complete_item(item_id):
         mark_item_done(item_id)
+
+        app.logger.info("Item completed")
         return redirect('/')
 
     return app
